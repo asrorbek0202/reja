b7f186dae9e9be53c825a786102af3a2abb2465b
Mannobjonov <mannobjonov@Mannobjonovs-MacBook-Air.local>
2026-08-21 19:14:45 +0900

    BRR: building express web server

---
+++ server.js
@@ -0,0 +1,33 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http")
+
+//1: Kirish fayllar 
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//2.Session code 
+
+//3. Views code 
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+//4.Routing code 
+
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({test: "success"})
+});
+
+app.get("/", function (req, res) {
+  res.render("harid");
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen (PORT, function () {
+console.log(`The server is running on port ${PORT}`);
+})
+
