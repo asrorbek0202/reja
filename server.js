96c1f14b93a99a5874e6d59bb1129cfb10f492d3
Mannobjonov <mannobjonov@Mannobjonovs-MacBook-Air.local>
2026-08-30 05:34:23 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -2,19 +2,6 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
-const fs = require("fs")
-
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if(err){
-    console.log("ERROR", err);
-  }else{
-    user = JSON.parse(data)
-  }
-})
-
-// Rejalarni saqlaydigan massiv (Array)
-let plans = ["Dasturlashni o'rganish", "Kitob o'qish"];
 
 // 1. Kirish fayllar
 app.use(express.static("public"));
@@ -27,35 +14,21 @@ app.set("view engine", "ejs");
 
 // 3. Routing code
 
-// READ: Bosh sahifada barcha rejalarni ko'rsatish
-app.get("/", function (req, res) {
-  res.render("harid", { items: plans });
+app.post("/create-item", (req, res) =>{
+  
+});
+
+app.get("/", (req, res) => {
+res.render("reja");
 });
 
 app.get("/author", function (req, res) {
   res.render("author", {user: user});
 });
 
-// CREATE: Yangi reja qo'shish
-app.post("/create-item", (req, res) => {
-  const newItem = req.body.item;
-  if (newItem && newItem.trim() !== "") {
-    plans.push(newItem.trim());
-  }
-  res.redirect("/");
-});
-
-// DELETE: Rejani o'chirish (ixtiyoriy)
-app.post("/delete-item", (req, res) => {
-  const index = req.body.index;
-  if (index !== undefined) {
-    plans.splice(index, 1);
-  }
-  res.redirect("/");
-});
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-  console.log(`The server is running on port ${PORT}`);
+  console.log(`The server is running on port ${PORT}, http://localhost:${PORT}`);
 });
--- train.js
+++ train.js
@@ -1,12 +1,12 @@
-console.log("Jack Ma maslahatlari:")
-const list = [
-    "yaxshi talaba boling", // 0-20
-    "togri boshliq tanlang va koproq xato qiling", // 20-30
-    "ozingizga ishlashni boshlang", // 30-40
-    "siz kuchli bolgan ishlarni qiling ", //40-50
-    "yoshlarga investitsiya qiling", // 50-60
-    "endi dam oling foydasi yoq :)", // 60 ~
-];
+// console.log("Jack Ma maslahatlari:")
+// const list = [
+//     "yaxshi talaba boling", // 0-20
+//     "togri boshliq tanlang va koproq xato qiling", // 20-30
+//     "ozingizga ishlashni boshlang", // 30-40
+//     "siz kuchli bolgan ishlarni qiling ", //40-50
+//     "yoshlarga investitsiya qiling", // 50-60
+//     "endi dam oling foydasi yoq :)", // 60 ~
+// ];
 
 
 
@@ -27,31 +27,31 @@ const list = [
 
 // setInterval calllbacklarda ishlayda qayta qayta lekin asynch funksiyalarda ishlamaydi
 
-async function maslahatBering(a) {
-    if (typeof a !== "number") throw new Error("insert a number");
-    else if (a <= 20) return list[0];
-    else if (a > 20 && a <= 30) return list[1];
-    else if (a > 30 && a <= 40) return list[2];
-    else if (a > 40 && a <= 50) return list[3];
-    else if (a > 50 && a <= 60) return list[4];
-    else {
-        return new Promise((resolve , reject){
-             setTimeout(() => {
-             resolve (list[5]);
-         }, 5000);
-        });
-    }
-}
+// async function maslahatBering(a) {
+//     if (typeof a !== "number") throw new Error("insert a number");
+//     else if (a <= 20) return list[0];
+//     else if (a > 20 && a <= 30) return list[1];
+//     else if (a > 30 && a <= 40) return list[2];
+//     else if (a > 40 && a <= 50) return list[3];
+//     else if (a > 50 && a <= 60) return list[4];
+//     else {
+//         return new Promise((resolve , reject){
+//              setTimeout(() => {
+//              resolve (list[5]);
+//          }, 5000);
+//         });
+//     }
+// }
 
-async function run() {
-    let javob = await maslahatBering(17);
-    console.log(javob);
-    javob = await maslahatBering(25);
-    console.log(javob);
-    javob = await maslahatBering(35);
-    console.log(javob);
-}
-run();
+// async function run() {
+//     let javob = await maslahatBering(17);
+//     console.log(javob);
+//     javob = await maslahatBering(25);
+//     console.log(javob);
+//     javob = await maslahatBering(35);
+//     console.log(javob);
+// }
+// run();
 
 // console.log("passed here 0");
 // maslahatBering(20)
@@ -62,9 +62,6 @@ run();
 //         console.log("ERROR:", err);
 //     });
 // console.log("passed here 1");
-
-
-
 
 
 // function maslahatBering(a, callback) {
